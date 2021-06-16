e426c623ec10d18db6d83cec1ff13f305aed9444
amyyyy06 <atoki@horizon.csueastbay.edu>
2021-06-16 14:10:59 +0900

    pro and post conditions

--- ImageMaker.h
+++ ImageMaker.h
@@ -26,39 +26,57 @@ public:
 
     // Opens image with filename and stores information into
     void LoadImage(string filename);
+    // Pre:
+    // Post:
     void SaveImage(string filename);
+    // Pre:
+    // Post:
 
     // Size functions
     int GetWidth();
     int GetHeight();
+    // Pre:  Image has been initialized.
+    // Post:
     void SetWidth(int width);
     void SetHeight(int height);
+    // Pre: Image has been initialized
+    // Post:
 
     // Color functions
     int GetPenRed();
     int GetPenGreen();
     int GetPenBlue();
+    // Pre:  Image has been initialized.
+    // Post:
     void SetPenRed(int newR);
     void SetPenGreen(int newG);
     void SetPenBlue(int newB);
+    // Pre: Image has been initialized
+    // Post:
 
     // Drawing methods
     // These methods will use the current red, green, blue values of the pen
     void DrawPixel(int x, int y);
+    // Pre: Image has been initialized
+    // Post:
     void DrawRectangle(int x1, int y1, int x2, int y2);
+    // Pre: Image has been initialized
+    // Post:
     void DrawLine(int x1, int y1, int x2, int y2);
+    // Pre: Image has been initialized
+    // Post:
 
 private:
     string magic;
-    int width;
-    int height;
-    int pen_red;    // Used by draw functions
-    int pen_green;  // Used by draw functions
-    int pen_blue;   // Used by draw functions
+    int width{};
+    int height{};
+    int pen_red{};    // Used by draw functions
+    int pen_green{};  // Used by draw functions
+    int pen_blue{};   // Used by draw functions
     bool PointInBounds(int x, int y);  // Not a bad idea to implement
 
                //   x          y
-    short image[MAX_WIDTH][MAX_HEIGHT][3];
+    short image[MAX_WIDTH][MAX_HEIGHT][3]{};
 };
 
 #endif //IMAGEMAKER_H
